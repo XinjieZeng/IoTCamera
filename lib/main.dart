124025c784c66a5ec6c63619db892e3dea7ee3cd
xinjiezeng@gmail.com <xinjiezeng@gmail.com>
2020-12-10 16:46:51 -0500

    fix bug in login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:hello_app/postLogin.dart';
 import 'package:hello_app/menu.dart';
+import 'package:dio/dio.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 void main() => runApp(new MyApp());
 
@@ -11,18 +12,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Example',
+      home:  MyCustomForm(), 
       // Routes
-      routes: <String, WidgetBuilder>{
-        '/': (_) => new Login(), // Login Page
+      routes: <String, WidgetBuilder>{ 
         '/mainmenu': (_) => new MainMenu(), // Home Page
       },
     );
   }
 }
 
+class MyCustomForm extends StatefulWidget{
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
 
-// The login page 
-class Login extends StatelessWidget {
+class _MyCustomFormState extends State<MyCustomForm> {
+// The login page
+  
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+ @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -50,6 +68,7 @@ class Login extends StatelessWidget {
             ),
 
             TextField(
+                 controller: usernameController,
                  decoration: const InputDecoration(
                    labelText: "Username",
                    labelStyle: TextStyle(
@@ -60,6 +79,7 @@ class Login extends StatelessWidget {
                ),
                 
             TextFormField(
+                 controller: passwordController,
                  decoration: const InputDecoration(
                    labelText: "password",
                    labelStyle: TextStyle(
@@ -70,9 +90,11 @@ class Login extends StatelessWidget {
                ),
             // The button on pressed, logs-in the user to and shows Home Page
             new RaisedButton(
+                child: new Text('login'),
                 onPressed: () =>
-                    Navigator.of(context).pushReplacementNamed("/mainmenu"),
-                child: new Text("Login")),
+                  postLogin(usernameController.text, passwordController.text, context),
+                    // Navigator.of(context).pushReplacementNamed("/mainmenu"),
+            ),
           ],
 
           
@@ -80,162 +102,54 @@ class Login extends StatelessWidget {
       ),
     );
   }
+
+
 }
 
 
 
-
-
-
-
-
-
-
-
-// void main()  {
-//     runApp(MyApp());
-//   }
-
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       home: MyLoginForm(),
-//     );
-//   }
+// postLogin(username, password) async{
+//    var params = Map<String, String> ();
+//    params["username"] = username;
+//    params["password"] = password;
+//     var client = http.Client();
+//     var response = await client.post('http://192.168.254.73:5000/login', body: params);
+//     print(response.body);
+//     // _content = response.body;
+    
 // }
 
-// class MyLoginForm extends StatefulWidget {
-//   @override
-//   _MyCustomFormState createState() => _MyCustomFormState();
-// }
+void postLogin(username, password, context) async {
+    FormData formData = FormData.fromMap({
+          'username': username, 
+          'password': password,
+      });
+
+    Dio dio = new Dio();
+    var response = await dio.post<String> (
+      "http://192.168.254.73:5000/login", 
+      data: formData,
+      );
+
+    if (response.statusCode == 200) {
+      if(response.data == "success") {
+         print("successful log in");
+         Navigator.of(context).pushReplacementNamed("/mainmenu");
+      }
+      else {
+          Fluttertoast.showToast(
+            msg: 'Login failed',
+            gravity: ToastGravity.CENTER,
+            textColor: Colors.grey
+          );
+      }
+    
+    }
+    
+  }
 
 
-// class _MyCustomFormState extends State<MyLoginForm> {
-
-//   final usernameController = TextEditingController();
-//   final passwordController = TextEditingController();
-//   Future<String> _futureLogin; 
-
-//   @override
-//   void dispose() {
-//     // Clean up the controller when the widget is disposed.
-//     usernameController.dispose();
-//     passwordController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//        return MaterialApp(
-  
-//       title: 'Flutter demo',
-//       home: 
-//       Scaffold(
-//         appBar: AppBar(),
-//         body: Container(
-//           margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 40.0),
-//           child:  (_futureLogin == null) 
-//             ? Column(
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//                Text(
-//                  "User Login",
-//                  style: TextStyle(
-//                    color: Colors.black54,
-//                    fontWeight: FontWeight.w800,
-//                    fontFamily:'Roboto',
-//                    letterSpacing:0.5,
-//                    fontSize: 30.0,
-//                  ),
-//                  ),
-//                  Icon(
-//                     Icons.login, 
-//                     color: Colors.black54,
-//                     size:40.0,
-//                  ),
-               
-//                Padding(
-//                  padding: const EdgeInsets.only(top: 50.0),
-//                ),
-
-//                TextField(
-//                  decoration: const InputDecoration(
-//                    labelText: "Username",
-//                    labelStyle: TextStyle(
-//                      fontSize: 20.0
-//                    )
-//                  ),
-//                  controller: usernameController
-//                ),
-                
-//                TextFormField(
-//                  decoration: const InputDecoration(
-//                    labelText: "password",
-//                    labelStyle: TextStyle(
-//                      fontSize: 20.0
-//                    )
-//                  ),
-//                  controller: passwordController,
-//                  obscureText: true,
-//                ),
-               
-//               Padding(
-//                  padding: const EdgeInsets.only(top: 30.0),
-//                ),
 
 
-//                RaisedButton(
-//                  child: const Text(  
-//                     "Login in", style: TextStyle(fontSize: 20)
-//                     ),
-//                   textColor: Colors.white54,
-//                   padding: const EdgeInsets.all(3.0),
-//                   color: Colors.black45,
 
-//                   onPressed: () {
-//                     setState(() {
-//                           createLogin(usernameController.text, passwordController.text);
-
-//                         });    
-//                   },
-                  
-//                ),
-//             ]
-//           )
-
-//           : FutureBuilder<String>(
-//                   future: _futureLogin,
-//                   builder: (context, snapshot) {
-//                     if (snapshot.hasData) {
-                    
-//                       if (snapshot.data == "invalid") {
-//                          //AlertDialog(title: Text("the password or username is not valid"));
-//                          Navigator.pop(context);
-//                       }
-//                       else {
-//                         Navigator.push(context,  MaterialPageRoute(builder: (context) => MainMenu()));
-//                       }  
-//                     } 
-                    
-//                     else if (snapshot.hasError) {
-//                       return Text("${snapshot.error}");
-//                     }
-//                     // else {
-//                     //     return CircularProgressIndicator();
-//                     // }    
-//                   },
-//                 ),
-//         ),
-//       ),
-//     );
-//   }
-
-
-// }
-  
-   
 
